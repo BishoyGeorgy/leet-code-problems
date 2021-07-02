7630a348ce3543e11940b58fee52877a8c8d1887
Bishoy Georgy <47426806+BishoyGeorgy@users.noreply.github.com>
2021-07-02 14:51:12 +0200

    [Strings] Reverse Array

---
+++ ReverseArrayString.java
@@ -0,0 +1,13 @@
+class ReverseArrayString {
+    public void reverseString(char[] s) {
+        for (int i = 0; i < s.length / 2; i++) {
+            swap(s, i, s.length - 1 - i);
+        }
+    }
+    
+    private void swap(char[] s, int index1, int index2) {
+        char temp = s[index1];
+        s[index1] = s[index2];
+        s[index2] = temp;
+    }
+}
